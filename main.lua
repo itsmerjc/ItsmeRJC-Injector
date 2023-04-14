472373a2bfad9c7c3399c9208c485d4d44f2032f
itsmerjc <95328291+itsmerjc@users.noreply.github.com>
2023-04-14 01:43:14 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,4 +4,4 @@ import "android.os.*"
 import "android.widget.*"
 import "android.view.*"
 
-    Toast.makeText(activity, "tree.",Toast.LENGTH_SHORT).show()
+    Toast.makeText(activity, "tre0000000000000000000000000e.",Toast.LENGTH_SHORT).show()
