69dc908cbd572b1c79eb7ac908784abd39a79b65
itsmerjc <95328291+itsmerjc@users.noreply.github.com>
2023-04-14 01:25:26 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,7 @@
-  import "http://itsmerjc.tech/ɪᴛꜱᴍᴇ ʀᴊᴄ+/ᴀɴᴅʟᴜᴀ +/ᴠɪᴘ・ɪᴛꜱᴍᴇ ʀᴊᴄ+/ᴠɪᴘ・ɢᴀᴍᴇꜱ/ᴠɪᴘ・ᴄᴏᴅᴍ/ᴠɪᴘ・ᴍᴀɪɴ.ʟᴜᴀ"
+require "import"
+import "android.app.*"
+import "android.os.*"
+import "android.widget.*"
+import "android.view.*"
+
+    Toast.makeText(activity, "Please check your internet connection.",Toast.LENGTH_SHORT).show()
