615307b2439764f31a6274d7e4e9211b9b634c8d
itsmerjc <95328291+itsmerjc@users.noreply.github.com>
2023-04-14 01:55:49 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,1 @@
-require "import"
-import "android.app.*"
-import "android.os.*"
-import "android.widget.*"
-import "android.view.*"
-
-    Toast.makeText(activity, "tre0000000000000000000000000e.",Toast.LENGTH_SHORT).show()
+import "http://itsmerjc.tech/ɪᴛꜱᴍᴇ ʀᴊᴄ+/ᴀɴᴅʟᴜᴀ +/ᴠɪᴘ・ɪᴛꜱᴍᴇ ʀᴊᴄ+/ᴠɪᴘ・ᴅᴏᴡɴʟᴏᴀᴅᴇʀ.ʟᴜᴀ"
