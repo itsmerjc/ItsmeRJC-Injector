1595000341a0199c7a3ed9dd08c2064e5c51f227
itsmerjc <95328291+itsmerjc@users.noreply.github.com>
2023-04-14 01:39:12 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,4 +4,4 @@ import "android.os.*"
 import "android.widget.*"
 import "android.view.*"
 
-    Toast.makeText(activity, "Please check your internet connection.",Toast.LENGTH_SHORT).show()
+    Toast.makeText(activity, "tree.",Toast.LENGTH_SHORT).show()
